1f60ce7e8821a26878d02324bfccd6d4442cf134
amandeep singh parihar <amandeepparihar24@gmail.com>
2023-10-27 22:38:46 +0530

    5

--- ARRAY/selection_sort.cpp
+++ ARRAY/selection_sort.cpp
@@ -3,20 +3,26 @@
 
 using namespace std;
 
-void selectionSort(vector<int>& arr, int n)
+void selectionSort(vector<int> &arr, int n)
 {
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n - 1; i++)
+    {
         int minIndex = i;
 
-        for (int j = i + 1; j < n; j++) {
+        for (int j = i + 1; j < n; j++)
+        {
             if (arr[j] < arr[minIndex])
                 minIndex = j;
         }
-        swap(arr[minIndex], arr[i]);
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
+        // swap(arr[minIndex], arr[i]);
     }
 }
 
-int main() {
+int main()
+{
     // Create a vector of integers to be sorted
     vector<int> arr = {12, 45, 23, 6, 56, 78, 34, 89, 13};
 
@@ -28,7 +34,8 @@ int main() {
 
     // Display the sorted vector
     cout << "Sorted array: ";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
