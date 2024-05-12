2f99cd245440a8d9e2b03d8e5aad751065c96b70
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2024-05-12 23:34:37 +0530

    test files

--- HASHING/1TwoSum.cpp
+++ HASHING/1TwoSum.cpp
@@ -1,28 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> twoSum(vector<int>&arr, int target)
+vector<int> twoSum(vector<int> &arr, int target)
 {
-    unordered_map<int,int>mpp;
-    for(int i=0;i<arr.size();++i)
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < arr.size(); ++i)
     {
-        int num=arr[i];
-        int required=target-num;
-        if(mpp.find(required)!=mpp.end())
+        int num = arr[i];
+        int required = target - num;
+        if (mpp.find(required) != mpp.end())
         {
-            return {mpp[required],i};
+            return {mpp[required], i};
         }
-        mpp[num]=i;
+        mpp[num] = i;
     }
-    return {-1,-1};
+    return {-1, -1};
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
-    vector<int> nums = {3,2,4};
+
+    vector<int> nums = {3, 2, 4};
     int target = 6;
     vector<int> result = twoSum(nums, target);
     cout << "Indices: [" << result[0] << ", " << result[1] << "]" << endl;
--- HASHING/xenortiy.cpp
+++ HASHING/xenortiy.cpp
@@ -7,11 +7,11 @@ long long maximumHappinessSum(vector<int> &v, int k)
     sort(v.begin(), v.end(), greater<int>());
 
     long long temp = 0;
-    for(int i=0;i<k;i++)
+    for (int i = 0; i < k; i++)
     {
-        temp+=v[i];
+        temp += v[i];
     }
-    return temp-(k-1);
+    return temp - (k - 1);
 }
 
 int main()
@@ -19,7 +19,7 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    vector<int> v = {1,1,1,1};
+    vector<int> v = {1, 1, 1, 1};
     cout << maximumHappinessSum(v, 2);
 
     return 0;
