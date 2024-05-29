89cac07dcca74747c83d4e60a5588112c56736d3
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2024-05-30 00:17:14 +0530

    sort 012

---
+++ ARRAY/sort 0 1 2/75SortColors_2.cpp
@@ -0,0 +1,42 @@
+//dutch national flag algorithm
+//dutch national flag algorithm
+//dutch national flag algorithm
+
+#include<bits/stdc++.h>
+using namespace std;
+static const bool __boost = []()
+{
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return ios_base::sync_with_stdio(false);
+}();
+
+void sortColors(vector<int>& nums)
+{
+    int low=0,mid=0,high=nums.size()-1;
+    while(mid<=high)
+    {
+        if(nums[mid]==0)
+        {
+            swap(nums[low],nums[mid]);
+            low++,mid++;
+        }else if(nums[mid]==1)
+        {
+            mid++;
+        }else{
+            swap(nums[mid],nums[high]);
+            high--;
+        }
+    }
+}
+
+int main()
+{
+    vector<int>nums={2,0,2,1,1,0};
+    sortColors(nums);
+    for(int i=0;i<nums.size();++i)
+    {
+        cout<<nums[i]<<" ";
+    }
+    return 0;
+}
