09fefa8d3dfba7010cc4ca44154e20ee9a90b086
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2025-02-20 23:07:17 +0530

    morris

--- TREES/6 Inorder Traversal.cpp
+++ TREES/6 Inorder Traversal.cpp
@@ -15,20 +15,20 @@ struct node
 };
 
 // Recursive
-// void inorder(node *root)
-// {
-//     if (root == NULL)
-//     {
-//         return;
-//     }
+void inorder(node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
 
-//     inorder(root->left);
-//     cout << root->data << " ";
-//     inorder(root->right);
-// }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
 
 // Iterative
-vector<int> inorder(node *root)
+vector<int> _inorder(node *root)
 {
     stack<node *> st; // we take a stack of node* type
     node *ele = root; // let the root node be ele
@@ -54,6 +54,37 @@ vector<int> inorder(node *root)
     return ans;
 }
 
+// Morris
+vector<int> __inorder(node *root)
+{
+    vector<int> ans;
+    if (!root)
+        return ans;
+    node *curr = root;   // take the root as current
+    while (curr != NULL) // till the current root not null
+    {
+        if (curr->left == NULL) // if the current root is null it means this is the left most root print it
+        {
+            ans.push_back(curr->data); // push it in the vector
+            curr = curr->right;        // now go the right of the current node
+        }
+        else // if the current->left is not null
+        {
+            node *leftChild = curr->left;    // the leftChild of the current
+            while (leftChild->right != NULL) // go to the most right of the left child
+            {
+                leftChild = leftChild->right;
+            }
+
+            leftChild->right = curr; // connect the most right node of leftChild to the current
+            node *temp = curr;       // get a temp as curr
+            curr = curr->left;       // go move the curr to the left
+            temp->left = NULL;       // now mark the left root as null as we don't go on it again
+        }
+    }
+    return ans; // return the ans;
+}
+
 int main()
 {
     node *root = new node(1);
@@ -61,11 +92,24 @@ int main()
     root->right = new node(3);
     root->left->left = new node(4);
     root->left->right = new node(5);
-    // inorder(root); // Inorder
-    vector<int> ans = inorder(root);
+
+    cout << "Recursive" << endl;
+    inorder(root);
+    cout << endl;
+
+    cout << "Iterative" << endl;
+    vector<int> ans = _inorder(root);
     for (int i = 0; i < ans.size(); ++i)
     {
         cout << ans[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Morris" << endl;
+    vector<int> res = __inorder(root);
+    for (int i = 0; i < res.size(); ++i)
+    {
+        cout << res[i] << " ";
     }
     cout << endl;
     return 0;
